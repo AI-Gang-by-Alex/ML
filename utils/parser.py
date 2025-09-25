6fb907419021908343b3fedc31d4e16ce5ace25a
alexborodach30 <sagopien04@gmail.com>
2025-09-25 11:25:28 +0300

    Parser init.

---
+++ utils/parser.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
+def open_site():
+    opts = Options()
+    opts.headless = False
+    opts.add_argument("--window-size=1200,800")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=opts)
+    driver.get("https://www.banki.ru/services/responses/list/")
+
+    time.sleep(5)
+    print("Title:", driver.title)
+    print("URL:", driver.current_url)
+    driver.quit()
+
+if __name__ == "__main__":
+    open_site()
